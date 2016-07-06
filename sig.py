e05b7f2cc81c78f052b9ebbc761d6d601e5512a6
Anya Nugent <anugent96@gmail.com>
2016-07-06 11:18:15 -0700

    Update sig.py

--- sig.py
+++ sig.py
@@ -1,3 +1,8 @@
+""" 
+Takes any 1 fits fle and prints RA, dec, fluxes (g, r, z, w1), masking value, type, and number of observations in z for when z flux
+is significantly positive (>2) and all other fluxes are not significant (<1).
+"""
+
 import sys
 image1 = str(sys.argv[1])
 from astropy.io import fits
@@ -9,31 +14,39 @@ DEC = tbl.field('dec') # DEC values
 fW = tbl.field('wise_flux') # model flux, WISE
 fD = tbl.field('decam_flux') # model flux, decam
 Mask = tbl.field('decam_anymask') #check this- any mask (for errors in telescope)
-d_nobs = tbl.field('decam_nobs')
+d_nobs = tbl.field('decam_nobs') # number of observations taken in each filter
 Type = tbl.field('type') # type of object
-ivd = tbl.field('decam_flux_ivar') # inverse variance (1/sigma squared)
-ivw = tbl.field('wise_flux_ivar')
+ivd = tbl.field('decam_flux_ivar') # inverse variance for decam (1/sigma squared)
+ivw = tbl.field('wise_flux_ivar')  # inverse variance for wise
 
 # Necessary Imports/ Functions
 import numpy as np
-g = np.array(fD[:,1])
-r = np.array(fD[:,2])
-z = np.array(fD[:,4])
-w1 = np.array(fW[:,0])
-z_nobs = d_nobs[:,4]
-g_sig2 = np.array(ivd[:,1])
-r_sig2 = np.array(ivd[:,2])
-z_sig2 = np.array(ivd[:,4])
-w1_sig2 = np.array(ivw[:,0])
+g = np.array(fD[:,1]) # g-flux
+r = np.array(fD[:,2]) # r-flux
+z = np.array(fD[:,4]) # z-flux
+w1 = np.array(fW[:,0]) # w1-flux
+z_nobs = d_nobs[:,4] # number of observations in z
+g_sig2 = np.array(ivd[:,1]) # g inverse variance
+r_sig2 = np.array(ivd[:,2]) # r inverse variance
+z_sig2 = np.array(ivd[:,4]) # z inverse variance
+w1_sig2 = np.array(ivw[:,0]) # w1 inverse variance
 aMask = [sum(x) for x in Mask] # see if any individual filter error is there for a candidate
 
-g_test = g*(g_sig2**0.5) 
+"""
+Flux times the squareroot of its corresponding inverse variance. Gives a better value for flux that can be used to find fluxes that
+are significant and not significant (ie: objects that exist and don't exist)
+"""
+g_test = g*(g_sig2**0.5)  
 r_test = r*(r_sig2**0.5)
 z_test = z*(z_sig2**0.5)
 w1_test = w1*(w1_sig2**0.5)
 
 
 # To print negative fluxes
+"""
+Finds RA, DEC, fluxes, masking values, and z_n-obs for when z-flux>2 and all other fluxes < 1
+"""
+
 RA1 = [x for (x, mask1, mask2) in zip(RA, w1_test, z_test) if mask1 < 1 and mask2 > 2]
 DEC1 = [x for (x, mask1, mask2) in zip(DEC, w1_test, z_test) if mask1 < 1 and mask2 > 2]
 g1 = [x for (x, mask1, mask2) in zip(g_test, w1_test, z_test) if mask1 < 1 and mask2 > 2]
@@ -56,10 +69,10 @@ type2 = [x for (x, mask1, mask2) in zip (type1, g1, r1) if mask1 < 1 and mask2 <
 z_nobs2 = [x for (x, mask1, mask2) in zip (z_nobs1, g1, r1) if mask1 < 1 and mask2 < 1]
 
 
-# /Users/anuge96/Downloads/ds9.darwinelcapitan.7.4/ds9 -zscale ~/Desktop/Fits\ Images/decals-1791p015-image-z.fits  -region ds9.reg    >> to circle all of these points
-# Region file format: DS9 version 4.1
-# global color=green dashlist=8 3 width=1 font="helvetica 10 normal" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1
-# fk5
+
+"""
+Prints these values for when masking value == 0
+"""
 
 print('RA , DEC, g, r, z, w1, mask, type, z-nobs')
 i = 0
@@ -79,6 +92,6 @@ while i < len(RA2) and i < len(DEC2) and i < len(g2) and i < len(r2) and i < len
 
     
     
-print('Num possible unmasked CRs=', len(RA2))    
+print('Num possible unmasked CRs=', len(RA2)) # prints total number of objects with the conditions above (z-flux signicant, others not)
 
 
